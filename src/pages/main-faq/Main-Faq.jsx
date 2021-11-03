bbff632af34da87f42cbe2e74a813a32468f1353
Carlos Andres Pasten Bravo <carlos92.pastenbravo@gmail.com>
2021-11-02 23:33:51 -0300

    order faq

--- src/pages/main-faq/Main-Faq.jsx
+++ src/pages/main-faq/Main-Faq.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
+import './Main-Faq.css'
 
 const MainFaq = () => {
     return (
-        <React.Fragment>
-            <div><h1>Faq: </h1></div>
-            <h1>¿Donde Estoy?</h1>
-            <article>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda saepe dolor vel fugiat unde, in quod dolorum eligendi placeat, maiores aspernatur nisi nesciunt aliquid ducimus ea quaerat sequi beatae qui.
+        <div className='faq'>
+            <h1> FAQ </h1>
+            <article className='where'>
+                <h1>¿Donde Estoy?</h1>
+                <div className='divisor'></div>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus optio dolores, aspernatur atque fuga architecto consectetur, magnam iste est ipsum magni nostrum omnis eaque temporibus distinctio obcaecati voluptas consequuntur autem? </p>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore autem, rem distinctio aut id voluptatum? Voluptas eaque modi, assumenda ab eveniet laboriosam nobis nemo aliquam ut! Dolore officiis impedit aliquid.</p>
             </article>
-            <h1> ¿Que hay aqui?</h1>
-            <article>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro eligendi hic at, veniam atque officia sunt sapiente dolores harum nihil omnis, possimus odio officiis necessitatibus eum delectus iste voluptates! Cum!
+
+
+            <article className='what'>
+                <h1>¿Que hay aqui?</h1>
+                <div className='divisor'></div>
+                <p> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur, minus dicta a enim delectus error modi facilis obcaecati id eveniet amet nemo deserunt debitis nisi tempora perferendis. Incidunt, asperiores harum?
+                </p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione officia non cum eum exercitationem quisquam dignissimos, recusandae quaerat tenetur impedit libero sequi? At id laborum eaque, eveniet ratione pariatur! Ratione.</p>
             </article>
-            <h1> ¿Que espero coseguir? </h1>
-            <article>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quo earum quidem ab cum id labore fugiat corporis quam, nam officia est possimus praesentium, qui laboriosam, necessitatibus ipsum sapiente dolor!
+
+            <article className='what-hope'>
+                <div className='divisor'></div>
+                <h1> ¿Que espero coseguir? </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum ipsa, tempore quas assumenda sunt voluptates deleniti alias et possimus ullam repellendus neque explicabo in beatae repudiandae? Minima, commodi. Adipisci, nesciunt!</p>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis expedita iusto quibusdam dicta magnam veniam recusandae quos, dignissimos soluta laudantium vel magni aspernatur at suscipit! Necessitatibus sunt ut perferendis maiores?</p>
             </article>
-        </React.Fragment>
+        </div>
     );
 }
 
